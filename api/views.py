cf471db9f9f1a1251b3db527c06d2a9b124e03fe
Claudiu <clau_123@yahoo.com>
2022-07-30 17:15:43 +0300

    create api endpoint for getting neighhourhoods

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from xml.etree import ElementTree
 
-# Create your views here.
+# DONT FORGET TO REMOVE xmlns="http://www.opengis.net/kml/2.2" FROM <kml> TAG
+
+def getNeighbourhoods(request):
+  dom = ElementTree.parse('Cartiere.kml').getroot()
+
+  cartiere = []
+  for item in dom.findall('Document/Placemark'):
+    cartier = {}
+    cartier['name'] = item.find('name').text
+
+    coordinatesStr = item.find('Polygon/outerBoundaryIs/LinearRing/coordinates').text.replace('\t', '').replace('\n', '').split(' ')
+    coordinates = []
+
+    for point in coordinatesStr:
+      if point:
+        coordinates.append({
+          'longitude': float(point.split(',')[0]),
+          'latitude': float(point.split(',')[1])
+        })
+      
+    cartier['coordinates'] = coordinates
+    cartiere.append(cartier)
+
+  return JsonResponse({'data': cartiere})
