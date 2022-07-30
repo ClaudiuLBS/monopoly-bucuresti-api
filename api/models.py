b28d0cc7bc2da4968454f2b69c029e1eaeea2dc0
Claudiu <clau_123@yahoo.com>
2022-07-30 17:25:03 +0300

    create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,40 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class GameSession(models.Model):
+  start_date    = models.DateTimeField(default=datetime.now())
+  code          = models.CharField(max_length=4)
+
+  def __str__(self) -> str:
+    return f'Game session {self.pk}'
+
+
+class NeighbourHood(models.Model):
+  name          = models.CharField(max_length=255)
+  color         = models.CharField(max_length=9)
+  price         = models.IntegerField(default=0)
+  rent          = models.IntegerField(default=0)
+  house_price   = models.IntegerField(default=0)
+  
+  def __str__(self) -> str:
+    return str(self.name)
+
+
+class Player(models.Model):
+  name          = models.CharField(max_length=255)
+  money         = models.IntegerField(default=1500)
+  owner         = models.BooleanField(default=False)
+  game_session  = models.ForeignKey(GameSession, on_delete=models.CASCADE)
+
+  def __str__(self) -> str:
+    return str(self.name)
+
+
+class Property(models.Model):
+  neighbourHood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE)
+  owner         = models.ForeignKey(Player, on_delete=models.CASCADE)
+  houses        = models.IntegerField(default=0)
+  
+  def __str__(self) -> str:
+    return f'{self.owner} - {self.neighbourHood}: {self.houses}'
