32761f31c3515794f3e9faa639f24da0c406f8c1
Claudiu <clau_123@yahoo.com>
2022-08-02 16:39:08 +0300

    add new api endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views.viewsets import PlayerViewSet, LandViewSet, PropertyViewSet, GameSessionViewSet
 from .views.session import create_session, join_session, start_session, end_session
-from .views.mechanics import buy_property, find_location, pay_rent
+from .views.mechanics import find_location, buy_property, attack_property, bring_soldiers, drop_soldiers
 from .views.info import all_players, lands_paths
 from rest_framework import routers
 
@@ -17,10 +17,12 @@ urlpatterns = [
   path('join-session/', join_session, name='join-session'),
   path('start-session/', start_session, name='start-session'),
   path('end-session/', end_session, name='end-session'),
-
+ 
   path('find-location/', find_location, name='find-location'),
   path('buy-property/', buy_property, name='buy-property'),
-  path('pay-rent/', pay_rent, name='pay-rent'),
+  path('attack-property/', attack_property, name='attack-property'),
+  path('bring-soldiers/', bring_soldiers, name='bring-soldiers'),
+  path('drop-soldiers/', drop_soldiers, name='drop-soldiers'),
 
   path('lands-paths/<int:code>', lands_paths, name='lands-paths'),
   path('all-players/<int:code>', all_players, name='all-players')
