6b90463652c54360268c90e34a52361ade2b20c6
Claudiu <clau_123@yahoo.com>
2022-08-02 15:29:00 +0300

    buy_property() takes 'property' instead of 'land' body argument

--- api/views/mechanics.py
+++ api/views/mechanics.py
@@ -31,54 +31,29 @@ def find_location(request):
 @csrf_exempt
 @require_http_methods(["POST"])
 def buy_property(request):
+  """takes a player and a property, if the property is free, player buys it"""
   body_unicode = request.body.decode('utf-8')
   body = json.loads(body_unicode)
 
-  player_id = body['player_id'] 
-  land_id = body['land_id']
+  player_id = body['player'] 
+  property_id = body['property']
   
   player = Player.objects.get(pk=player_id)
-  land = Land.objects.get(pk=land_id)
-  
-  # If no money
-  if player.money < land.price:
-    return JsonResponse({'error': 'No money'})
-    
+  property = Property.objects.get(pk=property_id)
+
   # If land is owned
-  property = Property.objects.get(land=land, game_session = player.game_session)
   if property.owner:
     return JsonResponse({'error': 'Property unavailable'}) 
   
+  # If no money
+  if player.money < property.land.price:
+    return JsonResponse({'error': 'No money'})
+    
+  
   property.owner = player
   property.save()
-  player.money -= land.price
+  player.money -= property.land.price
   player.save()
   
   return JsonResponse({'property_id': property.pk})
 
-
-
-@csrf_exempt
-@require_http_methods(["POST"])
-def pay_rent(request):
-  body_unicode = request.body.decode('utf-8')
-  body = json.loads(body_unicode)
-
-  player_id = body['player_id'] 
-  land_id = body['land_id']
-  
-  player = Player.objects.get(pk=player_id)
-  land = Land.objects.get(pk=land_id)
-  property = Property.objects.get(land=land, game_session=player.game_session)
-  owner = property.owner
-
-  rent_price = land.rent
-  if player.money < rent_price:
-    return JsonResponse({'error': 'Not enough money to pay rent'})
-  
-  owner.money += rent_price
-  owner.save()
-  player.money -= rent_price
-  player.save()
-  
-  return JsonResponse({{'money': player.money}})
