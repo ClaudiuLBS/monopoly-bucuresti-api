055d74991a191570e9c338cdc2f8be24c1da8f18
Claudiu <clau_123@yahoo.com>
2022-08-10 10:43:29 +0300

    add leave session to routes

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views.viewsets import PlayerViewSet, LandViewSet, PropertyViewSet, GameSessionViewSet
-from .views.session import create_session, join_session, start_session, end_session
+from .views.session import create_session, join_session, start_session, end_session, leave_session
 from .views.mechanics import find_location, buy_property, attack_property, bring_soldiers, drop_soldiers, buy_factory, train_soldiers
 from .views.info import get_game_rules, lands_paths, player_stats, properties_of, property_info, top_players
 from rest_framework import routers
@@ -13,23 +13,24 @@ router.register(r'game-sessions', GameSessionViewSet)
 
 urlpatterns = [
   path('api/', include(router.urls)),
-  path('create-session/', create_session, name='create-session'),
-  path('join-session/', join_session, name='join-session'),
-  path('start-session/', start_session, name='start-session'),
-  path('end-session/', end_session, name='end-session'),
+  path('create-session/', create_session),
+  path('join-session/', join_session),
+  path('leave-session/', leave_session),
+  path('start-session/', start_session),
+  path('end-session/', end_session),
  
-  path('find-location/', find_location, name='find-location'),
-  path('buy-property/', buy_property, name='buy-property'),
-  path('attack-property/', attack_property, name='attack-property'),
-  path('bring-soldiers/', bring_soldiers, name='bring-soldiers'),
-  path('drop-soldiers/', drop_soldiers, name='drop-soldiers'),
-  path('buy-factory/', buy_factory, name='buy-factory'),
-  path('train-soldiers/', train_soldiers, name='buy-factory'),
+  path('find-location/', find_location),
+  path('buy-property/', buy_property),
+  path('attack-property/', attack_property),
+  path('bring-soldiers/', bring_soldiers),
+  path('drop-soldiers/', drop_soldiers),
+  path('buy-factory/', buy_factory),
+  path('train-soldiers/', train_soldiers),
 
-  path('lands-paths/<int:code>', lands_paths, name='lands-paths'),
-  path('top-players/<int:code>', top_players, name='top-players'),
-  path('properties-of/<int:id>', properties_of, name='properties-of'),
-  path('property-info/<int:id>', property_info, name='property-info'),
-  path('player-stats/<int:id>', player_stats, name='player-stats'),
-  path('game-rules/<int:session_id>', get_game_rules, name='game-rules'),
+  path('lands-paths/<int:code>', lands_paths),
+  path('top-players/<int:code>', top_players),
+  path('properties-of/<int:id>', properties_of),
+  path('property-info/<int:id>', property_info),
+  path('player-stats/<int:id>', player_stats),
+  path('game-rules/<int:session_id>', get_game_rules),
 ]
